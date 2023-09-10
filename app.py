2d3d269757eaa59c952b39718983b3d63fb04dc5
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-09-10 16:54:41 +0300

    New book page

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/newbook")
+def newbook():
+    return render_template("newbook.html")
+
+@app.route("/create", methods=["ADD"])
+def create():
+    return redirect("/")
