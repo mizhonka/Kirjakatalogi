28a248dddd777af53e8ef2c42bc449dea91329c8
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-09-18 16:26:46 +0300

    Added secret key

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import text
 from os import getenv
 
 app=Flask(__name__)
+app.secret_key=getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"]=getenv("DATABASE_URL")
 db=SQLAlchemy(app)
 
